089a8ec91f6b3235277a374d18cd1ca77ab76311
Tim <tbarry2@andrew.cmu.edu>
2021-07-06 18:56:15 -0400

    fix data_sync

--- data_sync/pull_data.sh
+++ data_sync/pull_data.sh
@@ -1,3 +1,3 @@
 source ~/.research_config
-rclone sync -i ${LOCAL_GLMEIV_DATA_DIR}private ${REMOTE_GLMEIV_DATA_DIR}private 
-rclone sync -i ${LOCAL_GLMEIV_DATA_DIR}public ${REMOTE_GLMEIV_DATA_DIR}public
+rclone sync -i ${REMOTE_GLMEIV_DATA_DIR}private ${LOCAL_GLMEIV_DATA_DIR}private
+# rclone copy -i ${REMOTE_GLMEIV_DATA_DIR}public ${LOCAL_GLMEIV_DATA_DIR}public
--- data_sync/push_data.sh
+++ data_sync/push_data.sh
@@ -1,3 +1,3 @@
 source ~/.research_config
-rclone copy -i ${LOCAL_GLMEIV_DATA_DIR}private ${REMOTE_GLMEIV_DATA_DIR}private 
-rclone copy -i ${LOCAL_GLMEIV_DATA_DIR}public ${REMOTE_GLMEIV_DATA_DIR}public
+rclone copy -i ${LOCAL_GLMEIV_DATA_DIR}private ${REMOTE_GLMEIV_DATA_DIR}private
+# rclone copy -i ${LOCAL_GLMEIV_DATA_DIR}public ${REMOTE_GLMEIV_DATA_DIR}public
