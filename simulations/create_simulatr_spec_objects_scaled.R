60605379330f792f315de87682ab42d7ae254eb0
Tim <tbarry2@andrew.cmu.edu>
2021-08-03 14:52:47 -0400

    increase glmeiv_fast wall time

--- simulations/create_simulatr_spec_objects_scaled.R
+++ simulations/create_simulatr_spec_objects_scaled.R
@@ -56,7 +56,7 @@ sim_spec_7 <- create_simulatr_specifier_object_v2(param_grid = param_grid,
                                                           fixed_params = fixed_params,
                                                           one_rep_times = list(generate_data_function = 5,
                                                                                thresholding = 10,
-                                                                               glmeiv_fast = 15,
+                                                                               glmeiv_fast = 30,
                                                                                glmeiv_slow = 160))
 to_save_7 <- paste0(sim_dir, "/sim_spec_7.rds")
 save_obj(sim_spec_7, to_save_7, overwrite)
